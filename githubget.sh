527d2fa73e9237ee37b7a33876b0ed34b5e6a7f9
RetailManagementSystems <100673284+RetailManagementSystems@users.noreply.github.com>
2022-11-18 09:08:27 +0100

    Add files via upload

---
+++ githubget.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+git clone https://github.com/RetailManagementSystems/retailpatches.git tmp/
+mv -v /home/pointmediauser/Vodafone/tmp/* /home/pointmediauser/Vodafone/
+rm -R tmp/
+chmod 777 -R /home/pointmediauser/Vodafone
